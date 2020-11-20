f2274f32a59cee23492b05991d0839b178a466dd
Tom Adam <tom.adam21@gmail.com>
2020-11-20 14:52:40 -0800

    add categories table

--- express-back-end/src/db/02_gigs.sql
+++ express-back-end/src/db/02_gigs.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS gigs CASCADE;
 CREATE TABLE gigs (
   id SERIAL PRIMARY KEY NOT NULL,
   contractor_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
 
   title TEXT NOT NULL,
   price INTEGER NOT NULL DEFAULT 0,
   description TEXT NOT NULL,
-  category TEXT NOT NULL,
   photo_one TEXT NOT NULL,
   photo_two TEXT NOT NULL,
   photo_three TEXT NOT NULL
