c1ec410ca5d1b65ef3dbbbfa1d8089b67d53c15c
Tom Adam <tom.adam21@gmail.com>
2020-11-19 19:16:38 -0800

    db setup

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,18 +3,28 @@ const App = Express();
 const BodyParser = require("body-parser");
 const PORT = 8080;
 
+// PG database client / connection setup
+const { Pool } = require("pg");
+const dbParams = require("./knexfile.js");
+const db = new Pool(dbParams.development.connection);
+db.connect();
+
+db.query("SELECT * FROM users;", (error, response) => {
+  console.log("error :", error);
+  console.log("response :", response);
+});
+
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static("public"));
 // Import Routers
-const gigs = require('./src/routes/gigs')
-const users = require('./src/routes/users')
+const gigs = require("./src/routes/gigs");
+const users = require("./src/routes/users");
 
 // API Router
 App.use("/api", gigs);
 App.use("/api", users);
-
 
 // Port Listening
 App.listen(PORT, () => {
---
+++ express-back-end/src/db/02_gigs.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS gigs CASCADE;
+CREATE TABLE gigs (
+  id SERIAL PRIMARY KEY NOT NULL,
+  contractor_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  price INTEGER NOT NULL DEFAULT 0,
+  description TEXT NOT NULL,
+  photo_one TEXT NOT NULL,
+  photo_two TEXT NOT NULL,
+  photo_three TEXT NOT NULL
+);
