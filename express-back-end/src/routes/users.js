794d9a53219da36447d399f3a62bb9f27fb1c286
Tom Adam <tom.adam21@gmail.com>
2020-11-21 11:06:25 -0800

    users db

--- express-back-end/src/routes/users.js
+++ express-back-end/src/routes/users.js
@@ -6,10 +6,30 @@ router.get("/users", (req, res) =>
   })
 );
 
-router.put("/users", (req, res) =>
+router.put("/users", (req, res) => {
   res.json({
     message: "Route to create a new user!",
-  })
-);
+  });
+  const user = req.body;
+
+  const queryParams = [
+    user.first_name,
+    user.last_name,
+    user.email,
+    user.password,
+    user.phone,
+    user.address,
+    user.city,
+    user.province,
+    user.phone,
+    user,
+  ];
+  db.query(
+    `
+    INSERT INTO users (first_name, last_name, email, password, phone, address, city, province, phone_number, avatar_img, bio, education)
+    `,
+    (err, res)
+  );
+});
 
 module.exports = router;
