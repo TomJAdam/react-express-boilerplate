02ef906990abfd7451238dbe86f3e4f1fdd94466
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-21 00:45:16 -0700

    IndexBottom component imported and rendered

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import Categories from "./Categories";
+import IndexBottom from './IndexBottom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+  root: {
+    width: '100vw',
+    height: '500px',
+    marginTop: '3rem',
+    paddingTop: '3rem',
+    backgroundColor: 'rgba(14, 226, 144, 0.2)',
+    display: 'flex',
+    flexDirection: 'column'
+  }
+
+}))
 
 
 export default function Home() {
+
+  const classes = useStyles();
+
   return (
-    <Categories/>
+      <div>
+        <Categories />
+        <div className={classes.root}>
+          <IndexBottom />
+        </div>
+      </div>
   )
 }
