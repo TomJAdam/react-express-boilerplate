bffd7b9b0cd91917204e80a2ba2e7d4b43269b08
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-22 02:08:29 -0700

    hard coded gigs removed - now getting from api

--- react-front-end/src/components/CategoryPage.js
+++ react-front-end/src/components/CategoryPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, useParams, Link, Switch } from 'react-router-dom';
 import GigPage from './GigPage';
 import GigGrid from './Gigs/GigGrid';
@@ -8,78 +8,21 @@ export default function CategoryPage() {
 
   const params = useParams();
   const category = params.category;
+  const [gigs, setGigs] = useState([]);
 
   useEffect(() => {
     axios.get(`/api/categories/${category}`)
     .then(response => {
-      console.log(response.data);
+      console.log(response);
       return axios.get(`/api/gigs/${response.data}`)
     })
     .then(response => {
       console.log(response);
+      setGigs(response.data);
     })
   },[])
 
   // Need all gigs in category from API here - pass these to GigGrid
-
-  const gigs = [
-    {
-      id: 1,
-      contractor_id: 1,
-      price: 200,
-      title: "I'll fix it!",
-      description:
-        "The first step is to style the InputBase component. Once it's styled, you can either use it directly as a text field or provide it to the select input property to have a select field.",
-      photo_one:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_two:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_three:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-    },
-    {
-      id: 2,
-      contractor_id: 2,
-      title: "I'll fix it!",
-      price: 200,
-      description:
-        "The first step is to style the InputBase component. Once it's styled, you can either use it directly as a text field or provide it to the select input property to have a select field.",
-      photo_one:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_two:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_three:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-    },
-    {
-      id: 3,
-      contractor_id: 3,
-      title: "I'll fix it!",
-      price: 200,
-      description:
-        "The first step is to style the InputBase component. Once it's styled, you can either use it directly as a text field or provide it to the select input property to have a select field.",
-      photo_one:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_two:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_three:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-    },
-    {
-      id: 4,
-      contractor_id: 4,
-      title: "I'll fix it!",
-      price: 200,
-      description:
-        "The first step is to style the InputBase component. Once it's styled, you can either use it directly as a text field or provide it to the select input property to have a select field.",
-      photo_one:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_two:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-      photo_three:
-        "https://images.unsplash.com/photo-1505798577917-a65157d3320a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80",
-    },
-  ];
 
   return (
     <div>
