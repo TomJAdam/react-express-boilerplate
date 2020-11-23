a7a387b521e0089075321ffc6ac287fcf2896e18
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-22 17:04:20 -0700

    user and gig data now on GigPage - small bug exists

--- react-front-end/src/components/GigPage.js
+++ react-front-end/src/components/GigPage.js
@@ -1,12 +1,29 @@
+import Axios from 'axios';
 import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function GigPage(props) {
 
+  const [gig, setGig] = useState(props.gig);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axios.get(`/api/users/${gig.contractor_id}`)
+    .then(response => {
+      console.log(response.data);
+      setUser(response.data[0])
+    })
+  },[])
+
   const params = useParams();
-  console.log(props.gig);
+  console.log(gig);
 
   return (
-  <h1>This is the page for gig {params.gig_id} owned by {props.gig.contractor_id}</h1>
+    <div>
+      <h1>This is the page for gig {gig.id} owned by user id {user.first_name}</h1>
+      <p>About {user.first_name}: {user.bio}</p>
+      <p>This is the price of the gig: {gig.price}</p>
+    </div>
   )
 }
