f9ae39e0d414b1f4ee05c6f41f81ef1b7fe1d33a
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-24 12:20:08 -0700

    required states declared

--- react-front-end/src/components/Chat.js
+++ react-front-end/src/components/Chat.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import queryString from 'query-string';
+import { UserCookie } from "../hooks/UserCookie";
 // import { UserCookie } from "../../hooks/UserCookie";
 // const { cookie, setCookie } = useContext(UserCookie);
 
@@ -8,13 +9,23 @@ import queryString from 'query-string';
 export default function Chat({ location }) {
 
   // API call to get all messages for particular conversation using conversation_id from url
+  // API call to get id's for conversation
+
+  const [conversationID, setConversationID] = useState(null);
+  const [room, setRoom] = useState('');
+  const [userID, setUserID] = useState(null);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = 'localhost:8080';
+
 
   useEffect(() => {
     const { conv_id } = queryString.parse(location.search);
-    console.log('inside of chat', conv_id);
+    setRoom(conv_id);
+    console.log('the room we are in', room)
   })
 
   return (
-    <h1>Here is the messaging page</h1>
+    <h1>We are on the chat page currently in room {room}</h1>
   )
 }
