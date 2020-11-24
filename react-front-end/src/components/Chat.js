7073cf81a9e253fe50b9469ab290e96d2f825150
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-24 14:22:47 -0700

    messaging components initialized

--- react-front-end/src/components/Chat.js
+++ react-front-end/src/components/Chat.js
@@ -2,8 +2,7 @@ import React, { useEffect, useContext, useState } from 'react';
 import queryString from 'query-string';
 import { UserCookie } from "../hooks/UserCookie";
 import io from 'socket.io-client';
-// import { UserCookie } from "../../hooks/UserCookie";
-// const { cookie, setCookie } = useContext(UserCookie);
+
 
 let socket;
 
@@ -11,28 +10,37 @@ export default function Chat({ location }) {
 
   // API call to get all messages for particular conversation using conversation_id from url
   // API call to get id's for conversation
-
+  const { cookie, setCookie } = useContext(UserCookie);
   const [conversationID, setConversationID] = useState(null);
-  const [room, setRoom] = useState('');
+  const [room, setRoom] = useState(null);
   const [userID, setUserID] = useState(null);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:8080';
 
+  console.log('current user', cookie.user.id);
+
+  useEffect(() => {
+  },[])
+
 
   useEffect(() => {
     const { conv_id } = queryString.parse(location.search);
+    console.log(conv_id);
     setRoom(conv_id);
+    console.log('room', room);
     socket = io(ENDPOINT);
     console.log(socket);
     console.log('the room we are in', room)
+
     return () => {
-      // socket.emit('disconnect')
       socket.off();
     }
   },[ENDPOINT, location.search])
 
   return (
-    <h1>We are on the chat page currently in room {room}</h1>
+    <div>
+     <h1>We are on the chat page currently in room {room}</h1>
+    </div>
   )
 }
