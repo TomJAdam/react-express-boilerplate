ecdc427cfcd0ba19edf2660d85dc612eaea04519
Tom Adam <tom.adam21@gmail.com>
2020-11-25 12:33:04 -0800

    schema change

--- express-back-end/src/db/seeds/gigs.sql
+++ express-back-end/src/db/seeds/gigs.sql
@@ -1,8 +1,8 @@
-INSERT INTO gigs (contractor_id, category_id, title, price, description, photo_one, photo_two, photo_three)
+INSERT INTO gigs (contractor_id, category_id, title, price, description, photo_one)
 VALUES
-(1, 1, 'Plumbing', 200, 'test description', 'url1', 'url2', 'url3'),
-(1, 1, 'Plumbing', 200, 'test description', 'url1', 'url2', 'url3'),
-(1, 2, 'Hardwood', 200, 'test description', 'url1', 'url2', 'url3'),
-(1, 5, 'Electrical', 200, 'test description', 'url1', 'url2', 'url3'),
-(2, 4, 'Renovations', 200, 'test description', 'url1', 'url2', 'url3'),
-(3, 3, 'Landscaping', 200, 'test description', 'url1', 'url2', 'url3');
+(1, 1, 'Plumbing', 200, 'test description', 'url1'),
+(1, 1, 'Plumbing', 200, 'test description', 'url1'),
+(1, 2, 'Hardwood', 200, 'test description', 'url1'),
+(1, 5, 'Electrical', 200, 'test description', 'url1'),
+(2, 4, 'Renovations', 200, 'test description', 'url1'),
+(3, 3, 'Landscaping', 200, 'test description', 'url1');
