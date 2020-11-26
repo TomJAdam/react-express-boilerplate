83dc295a38e452bc687799241d463f7cdf7389b8
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-25 18:39:16 -0700

    styling updates to message form

--- react-front-end/src/components/Input.js
+++ react-front-end/src/components/Input.js
@@ -4,16 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   form: {
     display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-    maxHeight: '10%',
+    marginTop: '1.2rem'
+    // justifyContent: 'space-between',
+    // width: '100%',
+    // maxHeight: '10%',
   },
 
   input: {
     height: '2rem',
     width: '80%',
     border: '1px solid #E3E3E3',
-    borderRadius: '20px',
+    borderRadius: '20px 0px 0px 20px',
     padding: '10px',
     '&:focus': {
       outline: 'none'
@@ -23,9 +24,15 @@ const useStyles = makeStyles((theme) => ({
   button: {
     width: '100px',
     border: 'none',
-    borderRadius: '20px',
+    borderRadius: '0px 20px 20px 0px',
     background: '#0EE290',
-    color: 'white'
+    color: 'white',
+    '&:focus': {
+      outline: 'none',
+    },
+    '&:hover': {
+      cursor: 'pointer'
+    }
   }
 }));
 
